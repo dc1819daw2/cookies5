43ba06792b157e74b648ef584b8e2229285d5a67
dcm1819daw2 <dcm1819daw2@gmail.com>
2019-03-25 00:40:53 +0100

    cookies5 commit1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+setcookie("nom","Jaume Pons",time()+3600,"/","",0);
+setcookie("edat","21",time()+3600,"/","",0);
+setcookie("cicle","DAW",time()+3600,"/","",0);
+setcookie("curs","2n",time()+3600,"/","",0);
+setcookie("any","2018-2019",time()+3600,"/","",0);
+echo "HEROKU I ZEND SERVER<br>";
+echo "Afegint 5 cookies<br>";
+echo "Soc dc1819daw2<br>"; 
+?>
